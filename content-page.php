bdcd918dad9720c567179ae2db1d46769c83a0e4
cbdweb <nik@cbdweb.net>
2015-11-18 09:07:02 +1100

    serif font; font proportion;

--- content-page.php
+++ content-page.php
@@ -17,11 +17,9 @@
 	<div class="entry-content">
             <img class="featured-image" src="<?=wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' )[0]?>"/>
             <header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
             </header><!-- .entry-header -->
             <?php the_content(); ?>
 	</div><!-- .entry-content -->
-
-	<?php edit_post_link( __( 'Edit', 'twentyfifteen' ), '<footer class="entry-footer"><span class="edit-link">', '</span></footer><!-- .entry-footer -->' ); ?>
 
 </article><!-- #post-## -->
