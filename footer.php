f46163bc2f4577198b85bc149e254b02e78bef74
cbdweb <nik@cbdweb.net>
2015-11-17 17:29:42 +1100

    theme layout;

--- footer.php
+++ footer.php
@@ -1,5 +1,12 @@
 </div> <!-- content -->
+<br class="clearall" />
+
 </div> <!-- page -->
+
+<div id="footer">
+    <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
+    <div id="copyright">Site by <a href="http://www.cbdweb.net">CBDWeb</a></div>
+</div>
 <?php wp_footer()?>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -3,12 +3,17 @@
 /* load CSS */
 function simpleblock_scripts() {
     wp_enqueue_style( 'main', get_template_directory_uri() . '/style.css', array(), '0.9' );
+    if ( is_admin_bar_showing() )
+        wp_enqueue_style( 'admin-header', get_template_directory_uri() . '/admin-style.css', array(), '0.9' );
 }
 add_action( 'wp_enqueue_scripts', 'simpleblock_scripts' );
 
 /* register menus */
 function simpleblock_register_menu() {
-  register_nav_menu('header-menu',__( 'Header Menu' ));
+  register_nav_menu('header-menu',__( 'Header Menu' ) );
+  register_nav_menu('footer-menu', __( 'Footer Menu' ) );
 }
 add_action( 'init', 'simpleblock_register_menu' );
+
+add_theme_support( 'post-thumbnails' );
 
