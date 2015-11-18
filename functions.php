ddc9cf45239d59b9eec586a9deee60cd2b9c837c
cbdweb <nik@cbdweb.net>
2015-11-19 09:39:52 +1100

    google font Quattrocento

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 /* load CSS */
 function simpleblock_scripts() {
     wp_enqueue_style( 'main', get_template_directory_uri() . '/style.css', array(), '0.9' );
+    wp_enqueue_style( 'google', 'https://fonts.googleapis.com/css?family=Quattrocento:400,700', array(), '0.9');
     if ( is_admin_bar_showing() )
         wp_enqueue_style( 'admin-header', get_template_directory_uri() . '/admin-style.css', array(), '0.9' ); 
 }
