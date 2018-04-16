7eb7c60d1f369f788a215fee9e37195b7e7af566
cbdweb <nik@cbdweb.net>
2018-04-16 17:30:09 +1000

    comments template;

--- comments.php
+++ comments.php
@@ -1,75 +1,61 @@
 <?php
 /**
- * The template for displaying comments.
+ * The template for displaying Comments
+ * ngd - 15/4/2018 - borrowed from another theme to stop warnings about mandatory comments template.
  *
  * The area of the page that contains both current comments
- * and the comment form.
+ * and the comment form. The actual display of comments is
+ * handled by a callback to twentytwelve_comment() which is
+ * located in the functions.php file.
  *
- * @package thim
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
  */
+
 /*
  * If the current post is protected by a password and
  * the visitor has not yet entered the password we will
  * return early without loading the comments.
  */
-if ( post_password_required() ) {
+if ( post_password_required() )
 	return;
-}
-
-if (
-		have_comments() &&
-		!( !comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) )
-) {
-	$class_has_comments = " has-comments";
-} else {
-	$class_has_comments = "";
-}
 ?>
 
-<div id="comments" class="comments-area<?php echo esc_attr( $class_has_comments ); ?>">
-	<?php // You can start editing here -- including this comment!  ?>
+<div id="comments" class="comments-area">
+
+	<?php // You can start editing here -- including this comment! ?>
+
 	<?php if ( have_comments() ) : ?>
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through  ?>
-			<nav id="comment-nav-above" class="comment-navigation" role="navigation">
-				<h1 class="screen-reader-text"><?php esc_attr_e( 'Comment navigation', 'sailing' ); ?></h1>
+		<h2 class="comments-title">
+			<?php
+				printf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'twentytwelve' ),
+					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+			?>
+		</h2>
 
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( '&larr; Older Comments', 'sailing' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments &rarr;', 'sailing' ) ); ?></div>
-			</nav><!-- #comment-nav-above -->
-		<?php endif; // check for comment navigation  ?>
+		<ol class="commentlist">
+			<?php wp_list_comments( array( 'callback' => 'twentytwelve_comment', 'style' => 'ol' ) ); ?>
+		</ol><!-- .commentlist -->
 
-		<ol class="comment-list">
-			<div class="comment-list-inner">
-				<h2 class="comments-title">
-					<?php
-					printf( _nx( '1 Comment', '%1$s Comments', get_comments_number(), 'comments title', 'sailing' ), number_format_i18n( get_comments_number() ));
-					?>
-				</h2>
-				<?php wp_list_comments( 'style=li&&type=comment&avatar_size=90&callback=thim_comment' ); ?>
-			</div>
-		</ol><!-- .comment-list -->
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through  ?>
-			<nav id="comment-nav-below" class="comment-navigation" role="navigation">
-				<h1 class="screen-reader-text"><?php esc_attr_e( 'Comment navigation', 'sailing' ); ?></h1>
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+		<nav id="comment-nav-below" class="navigation" role="navigation">
+			<h1 class="assistive-text section-heading"><?php _e( 'Comment navigation', 'twentytwelve' ); ?></h1>
+			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'twentytwelve' ) ); ?></div>
+			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'twentytwelve' ) ); ?></div>
+		</nav>
+		<?php endif; // check for comment navigation ?>
 
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( '&larr; Older Comments', 'sailing' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments &rarr;', 'sailing' ) ); ?></div>
-			</nav><!-- #comment-nav-below -->
-		<?php endif; // check for comment navigation  ?>
+		<?php
+		/* If there are no comments and comments are closed, let's leave a note.
+		 * But we only want the note on posts and pages that had comments in the first place.
+		 */
+		if ( ! comments_open() && get_comments_number() ) : ?>
+		<p class="nocomments"><?php _e( 'Comments are closed.' , 'twentytwelve' ); ?></p>
+		<?php endif; ?>
+
 	<?php endif; // have_comments() ?>
-	<?php
-	// If comments are closed and there are comments, let's leave a little note, shall we?
-	if ( !comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
-		?>
-		<!-- <p class="no-comments" style="background: #fff;padding: 30px;"><?php esc_attr_e( 'Comments are closed.', 'sailing' ); ?></p> -->
-	<?php endif; ?>
-	<div class="comment-respond-area">
-		<?php comment_form( array(
-				'title_reply' => esc_html__( 'Leave A Reply', 'sailing' ),
-				'label_submit' => esc_html__( 'Post Comment', 'sailing' ),
-				'comment_field' => '<p class="comment-form-comment"><textarea placeholder="' . esc_attr__( 'Comment', 'sailing' ) . '" id="comment" name="comment" cols="45" rows="8" aria-required="true">' . '</textarea></p>'
-		) ); ?>
-	</div>
-	<div class="clear"></div>
 
-</div><!-- #comments -->
+	<?php comment_form(); ?>
+
+</div><!-- #comments .comments-area -->
