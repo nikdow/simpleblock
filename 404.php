0435c09110806e99476bfb205f15782989204621
nikdow <nik@cbdweb.net>
2023-01-02 18:37:27 +1100

    add 404 page template;

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The 404 error template
+ */
+
+get_header(); ?>
+<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+            <h2>Page does not exist</h2>
+	</div><!-- .content-area -->
+<?php
+get_footer();
+
